d70f74a469dff8d6258783168bc97b188e88a266
Jonathan Carpio <101604637+jona70x@users.noreply.github.com>
2023-07-31 14:15:46 -0700

    Add sorting, limiting, pagination, add alising functionality. Refactored API functionality to make it reusable. Aggregates pipeline to match and group results

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,30 +1,25 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+// Api features (delete later)
 
 exports.getAllTours = async (req, res) => {
   try {
-    // build query
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((field) => delete queryObj[field]);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // execute query
-    // 2) Advanced filtering
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`
-    );
-    const query = Tour.find(JSON.parse(queryString));
-    console.log(req.query);
-    console.log(JSON.parse(queryString));
+    console.log(features.query, features.queryString);
 
-    // const tours = await Tour.find()
-    //   .where('difficulty')
-    //   .equals('easy')
-    //   .where('duration')
-    //   .equals(5);
-
-    const tours = await query;
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -32,9 +27,15 @@ exports.getAllTours = async (req, res) => {
       data: { tours },
     });
   } catch (error) {
-    res.status(404).json({ status: 'failed', message: error });
+    res.status(404).json({ status: 'failed', message: error.message });
   }
 };
+
+// const tours = await Tour.find()
+//   .where('difficulty')
+//   .equals('easy')
+//   .where('duration')
+//   .equals(5);
 
 exports.getTour = async (req, res) => {
   try {
@@ -90,6 +91,57 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error.message,
+    });
+  }
+};
+
+// Adding aggregation pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingsAverage: { $gte: 4.5 },
+        },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: {
+            $sum: 1,
+          },
+          numRatings: {
+            $sum: '$ratingsQuantity',
+          },
+          avgRating: {
+            $avg: '$ratingsAverage',
+          },
+          avgPrice: {
+            $avg: '$price',
+          },
+          minPrice: {
+            $min: '$price',
+          },
+          maxPrice: {
+            $max: '$price',
+          },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: stats,
     });
   } catch (error) {
     res.status(404).json({
